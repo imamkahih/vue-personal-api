b6125f0ea79ddc7bb347cdc7166faa60aa6a64f6
imamkahih <immshahih20@gmail.com>
2022-03-02 21:33:20 +0700

    menambahkan navbar

---
+++ src/views/Register.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="register">
+    <h1>Register</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Register",
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Student.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="student">
+    <h2>Student</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+};
+</script>
+
+<style>
+</style>
